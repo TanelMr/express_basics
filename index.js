760d1039eaae6c0c97c502df204a76be1027488c
Tanel Märjama <tanel.marjama@khk.ee>
2022-02-05 13:33:06 +0200

    Public directory use

--- index.js
+++ index.js
@@ -2,16 +2,14 @@
 const express = require ('express');
 const app = express ();
 
+//public directory
+app.use(express.static('public'));
+
 //add listeners
 app.get('/user/:username',(req, res)=>{
     let user = req.params.username;
     res.render('index.ejs', {username:user});
 });
-
-app.get('*', (req, res)=> {
-    res.send('<h1>404, this page does not exist</h1><br><a href="/">Go to homepage</a>');
-});
-
 //listen on port 3000
 app.listen(3000, ()=>{
     console.log('Server started');
