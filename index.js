44f801be24980739bdae6ea3167f2fae3ecb19f4
Tanel Märjama <tanel.marjama@khk.ee>
2022-02-05 13:39:10 +0200

    Templating directory setup

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-//define express
+//define packages
 const express = require ('express');
+const path = require("path");
 const app = express ();
 
 //public directory
 app.use(express.static('public'));
 
+//add views template engine
+app.set('view engine', 'ejs');
+
+//add views directory path
+app.set('views', path.join(__dirname, 'views'));
+
+
 //add listeners
 app.get('/user/:username',(req, res)=>{
     let user = req.params.username;
-    res.render('index.ejs', {username:user});
+    res.render('index', {username:user});
 });
 //listen on port 3000
 app.listen(3000, ()=>{
